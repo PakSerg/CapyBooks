c47759a02860fbfa1e2178bd8fd8cec072fea299
Пак Сергей <sergegnabri2005@gmail.com>
2025-06-14 21:05:30 +0700

    Try to fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,13 +29,26 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-#     "http://localhost",
-#     "http://127.0.0.1",
-#     'http://5.129.207.86'
-# ]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 # Application definition
